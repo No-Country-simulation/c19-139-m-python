dad052392ebd66a4501a15f5b9eea1c2e795e49c
MiguelBG12 <miguel.bg12@gmail.com>
2024-07-12 18:13:32 -0500

    Project Member Table

---
+++ Commit/Database/Tables/project_members.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS Project_Members;
+
+CREATE TABLE Project_Members (
+    member_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    project_id INT,
+    role VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (project_id) REFERENCES Projects(project_id)
+);
