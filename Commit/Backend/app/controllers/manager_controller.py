31c4b206a2d3a52e16396ec32835f331b41ff266
MiguelBG12 <miguel.bg12@gmail.com>
2024-07-14 17:16:22 -0500

    Controller: Function for users assigned by the manager to a project

---
+++ Commit/Backend/app/controllers/manager_controller.py
@@ -0,0 +1,31 @@
+from app.config.db_conexion import data_conexion
+from app.models.user import UserCreateRequest, UserUpdateRequest, UserAssignRequest
+
+async def create_user(user_request: UserCreateRequest):
+    params = [
+        user_request.name,
+        user_request.email,
+        user_request.password
+    ]
+    result = data_conexion.execute_procedure('sp_create_user', params)
+    return result
+
+async def update_user(user_request: UserUpdateRequest):
+    params = [
+        user_request.user_id,
+        user_request.email,
+        user_request.password
+    ]
+    result = data_conexion.execute_procedure('sp_update_user_credentials', params)
+    return result
+
+async def assign_member(user_request: UserAssignRequest):
+    params = [
+        user_request.manager_id,
+        user_request.project_id,
+        user_request.member_name,
+        user_request.member_email,
+        user_request.member_password
+    ]
+    result = data_conexion.execute_procedure('sp_assign_member_to_project', params)
+    return result
