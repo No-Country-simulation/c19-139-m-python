13a0851491b245860d1497252e46710f85b9d224
MiguelBG12 <miguel.bg12@gmail.com>
2024-07-13 22:17:09 -0500

    With Pydantic the notification model is defined to validate and structure the data in the application

---
+++ Commit/Backend/app/models/notification.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class Notification(BaseModel):
+    notification_id: Optional[int]
+    user_id: int
+    content: str
+    is_read: Optional[bool] = False
+    created_at: Optional[str]
