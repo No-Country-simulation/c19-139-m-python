78920184e48633944f1f67c9b21114447ffab76a
MiguelBG12 <miguel.bg12@gmail.com>
2024-07-13 22:16:44 -0500

    With Pydantic the message model is defined to validate and structure the data in the application

---
+++ Commit/Backend/app/models/message.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class Message(BaseModel):
+    message_id: Optional[int]
+    sender_id: int
+    receiver_id: int
+    content: str
+    created_at: Optional[str]
