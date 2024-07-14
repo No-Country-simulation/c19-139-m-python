1ca0849f273f719215d50aa7397241a358eb6069
MiguelBG12 <miguel.bg12@gmail.com>
2024-07-13 22:16:29 -0500

    With Pydantic the document model is defined to validate and structure the data in the application

---
+++ Commit/Backend/app/models/document.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class Document(BaseModel):
+    document_id: Optional[int]
+    project_id: int
+    uploaded_by: int
+    title: str
+    file_path: str
+    created_at: Optional[str]
