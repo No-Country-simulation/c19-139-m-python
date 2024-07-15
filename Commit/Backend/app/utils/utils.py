f94e992b0ef00f59a7094b383f703a1c6c681377
MiguelBG12 <miguel.bg12@gmail.com>
2024-07-15 13:20:11 -0500

    Function to create token (JWT)

--- Commit/Backend/app/utils/utils.py
+++ Commit/Backend/app/utils/utils.py
@@ -1,3 +1,5 @@
+import jwt
+from datetime import datetime, timedelta
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
@@ -9,3 +11,10 @@ class Settings(BaseSettings):
         env_file = ".env"
 
 settings = Settings()
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=settings.access_token_expire_minutes)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=settings.algorithm)
+    return encoded_jwt
