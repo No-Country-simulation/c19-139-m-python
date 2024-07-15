8d0ddcd83fa739f9f0353015bc5018d5981d73c9
MiguelBG12 <miguel.bg12@gmail.com>
2024-07-15 12:55:21 -0500

    Endpoint to update data for an existing user

--- Commit/Backend/app/routes/manager.py
+++ Commit/Backend/app/routes/manager.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, HTTPException, Depends
 from pydantic import BaseModel
-from app.models.user import UserCreateRequest
+from app.models.user import UserCreateRequest, UserUpdateRequest
 from app.controllers.manager_controller import (
-    create_user
+    create_user,
+    update_user
 )
 
 router = APIRouter()
@@ -11,3 +12,9 @@ router = APIRouter()
 @router.post("/users")
 async def route_create_user(user_request: UserCreateRequest):
     return await create_user(user_request)
+
+# Endpoint to update data for an existing user
+@router.put("/users")
+async def route_update_user_data(user_request: UserUpdateRequest):
+    return update_user(user_request)
+
