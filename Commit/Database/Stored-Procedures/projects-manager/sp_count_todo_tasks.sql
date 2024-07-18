28ec8d3ff884d7910387cb7f61916481bd7f088c
MiguelBG12 <miguel.bg12@gmail.com>
2024-07-17 19:23:53 -0500

    SP for sp count 'to do' tasks

---
+++ Commit/Database/Stored-Procedures/projects-manager/sp_count_todo_tasks.sql
@@ -0,0 +1,30 @@
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS sp_count_todo_tasks $$
+
+CREATE PROCEDURE sp_count_todo_tasks(
+    IN p_manager_id INT,
+    IN p_project_id INT
+)
+BEGIN
+    DECLARE project_manager INT;
+
+    SELECT manager_id INTO project_manager
+    FROM Projects
+    WHERE project_id = p_project_id;
+
+    IF p_manager_id = project_manager THEN
+        SELECT 
+            priority,
+            COUNT(*) AS task_count
+        FROM Tasks
+        WHERE project_id = p_project_id AND status = 'to do'
+        GROUP BY priority;
+
+    ELSE
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Only the manager who created the project can view tasks';
+    END IF;
+END$$
+
+DELIMITER ;
