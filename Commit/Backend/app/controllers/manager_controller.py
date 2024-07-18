0ce7f3f927353010acac67cd7c912f21ea4f0aa0
MiguelBG12 <miguel.bg12@gmail.com>
2024-07-17 22:16:16 -0500

    Create and assign functionalities are separated, now it will only assign members

--- Commit/Backend/app/controllers/manager_controller.py
+++ Commit/Backend/app/controllers/manager_controller.py
@@ -20,13 +20,13 @@ async def update_user(user_request: UserUpdateRequest):
     result = data_conexion.execute_procedure('sp_update_user_credentials', params)
     return result
 
-async def assign_member(user_request: UserAssignRequest):
+async def assign_member_to_project(manager_id: int, project_id: int, member_email: str, role: str, seniority: str):
     params = [
-        user_request.manager_id,
-        user_request.project_id,
-        user_request.member_name,
-        user_request.member_email,
-        user_request.member_password
+        manager_id,
+        project_id,
+        member_email,
+        role,
+        seniority
     ]
     result = data_conexion.execute_procedure('sp_assign_member_to_project', params)
     return result
