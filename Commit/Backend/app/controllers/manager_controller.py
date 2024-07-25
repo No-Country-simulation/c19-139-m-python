f1d9847fbdfbc2c47d7ee72a73b7244505488d68
MiguelBG12 <miguel.bg12@gmail.com>
2024-07-24 22:42:15 -0500

    Fix controller bug, and deleted function

--- Commit/Backend/app/controllers/manager_controller.py
+++ Commit/Backend/app/controllers/manager_controller.py
@@ -2,13 +2,14 @@ from app.config.db_conexion import data_conexion
 from app.models.user import User, UserCreateRequest, UserUpdateRequest, UserAssignRequest, SeniorityEnum
 from app.models.project import ProjectCreateRequest, Project
 from app.models.task import TaskCreateRequest
- 
+from app.models.project_member import ProjectMember
+
 
 async def update_user(user_request: UserUpdateRequest):
     params = [
-        user_request.user_id,
         user_request.email,
-        user_request.password
+        user_request.new_email,
+        user_request.new_password
     ]
     result = await data_conexion.execute_procedure('sp_update_user_credentials', params)
     return result
@@ -24,18 +25,7 @@ async def create_project(project_request: ProjectCreateRequest):
     result = await data_conexion.execute_procedure('sp_create_project', params)
     return result
 
-async def assign_member_to_project(manager_id: int, project_id: int, member_email: str, role: str, seniority: SeniorityEnum):
-    params = [
-        manager_id,
-        project_id,
-        member_email,
-        role,
-        seniority.value
-    ]
-    result = await data_conexion.execute_procedure('sp_assign_member_to_project', params)
-    return result
-
-async def list_projects(manager_id: int):
+async def enum_projects(manager_id: int):
     params = [manager_id]
     result = await data_conexion.execute_procedure('sp_list_manager_projects', params)
     return result
@@ -82,13 +72,18 @@ async def count_todo_tasks(manager_id: int, project_id: int):
     result = await data_conexion.execute_procedure('sp_count_todo_tasks', params)
     return result
 
-async def create_member(manager_id: int, member_request: UserCreateRequest):
+async def create_member(manager_id: int, projectmember_request: ProjectMember):
     params = [
         manager_id,
-        member_request.name,
-        member_request.email,
-        member_request.password
+        projectmember_request.project_id,
+        projectmember_request.member_name,
+        projectmember_request.member_email,
+        projectmember_request.member_password,
+        projectmember_request.role,
+        projectmember_request.seniority.value,
+        projectmember_request.availability.value
     ]
+    print(f"Parameters: {params}")
     result = await data_conexion.execute_procedure('sp_create_member', params)
     return result
 
@@ -98,9 +93,8 @@ async def create_task(task_request: TaskCreateRequest, manager_id):
         task_request.project_id,
         task_request.title,
         task_request.description,
-        task_request.assigned_to,
-        task_request.status,
-        task_request.priority,
+        task_request.status.value,
+        task_request.priority.value,
         task_request.start_date,
         task_request.due_date
     ]
