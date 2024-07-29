54e191aaf97f1791ca17fa4fcb85cd73ebd1e6ae
MiguelBG12 <miguel.bg12@gmail.com>
2024-07-28 22:08:39 -0500

    Now the tables in the excel file will be updated in real time

--- Commit/Backend/app/controllers/manager_controller.py
+++ Commit/Backend/app/controllers/manager_controller.py
@@ -1,18 +1,34 @@
 from app.config.db_conexion import data_conexion
-from app.models.user import User, UserCreateRequest, UserUpdateRequest, UserAssignRequest, SeniorityEnum
+from app.models.user import (
+    User,
+    UserCreateRequest,
+    UserUpdateRequest,
+    UserAssignRequest,
+    SeniorityEnum,
+)
 from app.models.project import ProjectCreateRequest, Project
 from app.models.task import TaskCreateRequest
 from app.models.project_member import ProjectMember
 
 
 async def update_user(user_request: UserUpdateRequest):
-    params = [
-        user_request.email,
-        user_request.new_email,
-        user_request.new_password
+    params = [user_request.email, user_request.new_email, user_request.new_password]
+    result = await data_conexion.execute_procedure("sp_update_user_credentials", params)
+
+    tables = [
+        "Users",
+        "Projects",
+        "Project_Members",
+        "Tasks",
+        "Documents",
+        "Reports",
+        "Comments",
+        "Messages",
+        "Notifications",
     ]
-    result = await data_conexion.execute_procedure('sp_update_user_credentials', params)
+    data_conexion.export_to_excel(tables)
     return result
+
 
 async def create_project(project_request: ProjectCreateRequest):
     params = [
@@ -20,57 +36,157 @@ async def create_project(project_request: ProjectCreateRequest):
         project_request.name,
         project_request.description,
         project_request.start_date,
-        project_request.due_date
+        project_request.due_date,
     ]
-    result = await data_conexion.execute_procedure('sp_create_project', params)
+    result = await data_conexion.execute_procedure("sp_create_project", params)
+
+    tables = [
+        "Users",
+        "Projects",
+        "Project_Members",
+        "Tasks",
+        "Documents",
+        "Reports",
+        "Comments",
+        "Messages",
+        "Notifications",
+    ]
+    data_conexion.export_to_excel(tables)
     return result
+
 
 async def enum_projects(manager_id: int):
     params = [manager_id]
-    result = await data_conexion.execute_procedure('sp_list_manager_projects', params)
+    result = await data_conexion.execute_procedure("sp_list_manager_projects", params)
+
+    tables = [
+        "Users",
+        "Projects",
+        "Project_Members",
+        "Tasks",
+        "Documents",
+        "Reports",
+        "Comments",
+        "Messages",
+        "Notifications",
+    ]
+    data_conexion.export_to_excel(tables)
     return result
+
 
 async def delete_member(user_id: int):
     params = [user_id]
-    result = await data_conexion.execute_procedure('sp_delete_member', params)
+    result = await data_conexion.execute_procedure("sp_delete_member", params)
+
+    tables = [
+        "Users",
+        "Projects",
+        "Project_Members",
+        "Tasks",
+        "Documents",
+        "Reports",
+        "Comments",
+        "Messages",
+        "Notifications",
+    ]
+    data_conexion.export_to_excel(tables)
     return result
+
 
 async def project_details(project_id: int, manager_id: int):
     params = [project_id, manager_id]
-    result = await data_conexion.execute_procedure('sp_view_project_details', params)
+    result = await data_conexion.execute_procedure("sp_view_project_details", params)
+
+    tables = [
+        "Users",
+        "Projects",
+        "Project_Members",
+        "Tasks",
+        "Documents",
+        "Reports",
+        "Comments",
+        "Messages",
+        "Notifications",
+    ]
+    data_conexion.export_to_excel(tables)
     return result
+
 
 async def assign_task(task_id: int, assigned_to: int):
-    params = [
-        task_id,
-        assigned_to
+    params = [task_id, assigned_to]
+    result = await data_conexion.execute_procedure("sp_assign_task", params)
+
+    tables = [
+        "Users",
+        "Projects",
+        "Project_Members",
+        "Tasks",
+        "Documents",
+        "Reports",
+        "Comments",
+        "Messages",
+        "Notifications",
     ]
-    result = await data_conexion.execute_procedure('sp_assign_task', params)
+    data_conexion.export_to_excel(tables)
     return result
+
 
 async def count_completed_tasks(manager_id: int, project_id: int):
-    params = [
-        manager_id,
-        project_id
+    params = [manager_id, project_id]
+    result = await data_conexion.execute_procedure("sp_count_completed_tasks", params)
+
+    tables = [
+        "Users",
+        "Projects",
+        "Project_Members",
+        "Tasks",
+        "Documents",
+        "Reports",
+        "Comments",
+        "Messages",
+        "Notifications",
     ]
-    result = await data_conexion.execute_procedure('sp_count_completed_tasks', params)
+    data_conexion.export_to_excel(tables)
     return result
+
 
 async def count_in_progress_tasks(manager_id: int, project_id: int):
-    params = [
-        manager_id,
-        project_id
+    params = [manager_id, project_id]
+    result = await data_conexion.execute_procedure("sp_count_in_progress_tasks", params)
+
+    tables = [
+        "Users",
+        "Projects",
+        "Project_Members",
+        "Tasks",
+        "Documents",
+        "Reports",
+        "Comments",
+        "Messages",
+        "Notifications",
     ]
-    result = await data_conexion.execute_procedure('sp_count_in_progress_tasks', params)
+    data_conexion.export_to_excel(tables)
     return result
 
+
 async def count_todo_tasks(manager_id: int, project_id: int):
-    params = [
-        manager_id,
-        project_id
+    params = [manager_id, project_id]
+    result = await data_conexion.execute_procedure("sp_count_todo_tasks", params)
+
+    tables = [
+        "Users",
+        "Projects",
+        "Project_Members",
+        "Tasks",
+        "Documents",
+        "Reports",
+        "Comments",
+        "Messages",
+        "Notifications",
     ]
-    result = await data_conexion.execute_procedure('sp_count_todo_tasks', params)
+    data_conexion.export_to_excel(tables)
     return result
+
 
 async def create_member(manager_id: int, projectmember_request: ProjectMember):
     params = [
@@ -81,11 +197,25 @@ async def create_member(manager_id: int, projectmember_request: ProjectMember):
         projectmember_request.member_password,
         projectmember_request.role,
         projectmember_request.seniority.value,
-        projectmember_request.availability.value
+        projectmember_request.availability.value,
     ]
     print(f"Parameters: {params}")
-    result = await data_conexion.execute_procedure('sp_create_member', params)
+    result = await data_conexion.execute_procedure("sp_create_member", params)
+
+    tables = [
+        "Users",
+        "Projects",
+        "Project_Members",
+        "Tasks",
+        "Documents",
+        "Reports",
+        "Comments",
+        "Messages",
+        "Notifications",
+    ]
+    data_conexion.export_to_excel(tables)
     return result
+
 
 async def create_task(task_request: TaskCreateRequest, manager_id):
     params = [
@@ -96,15 +226,56 @@ async def create_task(task_request: TaskCreateRequest, manager_id):
         task_request.status.value,
         task_request.priority.value,
         task_request.start_date,
-        task_request.due_date
+        task_request.due_date,
     ]
-    result = await data_conexion.execute_procedure('sp_create_task', params)
+    result = await data_conexion.execute_procedure("sp_create_task", params)
+
+    tables = [
+        "Users",
+        "Projects",
+        "Project_Members",
+        "Tasks",
+        "Documents",
+        "Reports",
+        "Comments",
+        "Messages",
+        "Notifications",
+    ]
+    data_conexion.export_to_excel(tables)
     return result
+
 
 async def get_busy_members():
-    result = await data_conexion.execute_procedure('sp_get_busy_members')
+    result = await data_conexion.execute_procedure("sp_get_busy_members")
+
+    tables = [
+        "Users",
+        "Projects",
+        "Project_Members",
+        "Tasks",
+        "Documents",
+        "Reports",
+        "Comments",
+        "Messages",
+        "Notifications",
+    ]
+    data_conexion.export_to_excel(tables)
     return result
 
+
 async def get_free_members():
-    result = await data_conexion.execute_procedure('sp_get_free_members')
+    result = await data_conexion.execute_procedure("sp_get_free_members")
+
+    tables = [
+        "Users",
+        "Projects",
+        "Project_Members",
+        "Tasks",
+        "Documents",
+        "Reports",
+        "Comments",
+        "Messages",
+        "Notifications",
+    ]
+    data_conexion.export_to_excel(tables)
     return result
--- Commit/Backend/app/controllers/member_controller.py
+++ Commit/Backend/app/controllers/member_controller.py
@@ -4,13 +4,23 @@ from app.models.task import EditTaskRequest
 
 router = APIRouter()
 
-async def edit_task(task_id: int, request: EditTaskRequest):
-   params = [
-      task_id,
-      request.assigned_member_id,
-      request.status.value
-   ]
-   print(f"Parameters: {params}")
-   result = await data_conexion.execute_procedure('sp_edit_task', params)
-   return result
 
+async def edit_task(task_id: int, request: EditTaskRequest):
+    params = [task_id, request.assigned_member_id, request.status.value]
+    print(f"Parameters: {params}")
+    result = data_conexion.execute_procedure("sp_edit_task", params)
+
+    tables = [
+        "Users",
+        "Projects",
+        "Project_Members",
+        "Tasks",
+        "Documents",
+        "Reports",
+        "Comments",
+        "Messages",
+        "Notifications",
+    ]
+    data_conexion.export_to_excel(tables)
+
+    return result
